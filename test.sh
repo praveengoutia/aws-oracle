89ff2ce6eb63c4fda4666060944afaa3bb42584e
praveengoutia <praveen.goutia@rackspace.com>
2017-08-29 22:03:53 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,52 @@
 #File name: test.sh
+##Format disk xvdb
+TGTDEV=xvdb
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +100G # /u01 partition
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+  +16G # Swap partition
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+
+##Format disk xvdc
+TGTDEV=xvdc
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+	# default, extend partition to end of disk
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+
+##Format disk xvdd
+TGTDEV=xvdd
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+	# default, extend partition to end of disk
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+
+##Create Swap 
+mkswap /dev/xvdb2>/tmp/swapon.1
+swapuuid=`grep UUID /tmp/swapon.1|cut -f2 -d =`
+swapon -U ${swapuuid}
