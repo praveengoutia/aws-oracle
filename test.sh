b8eeedb592810b6c43ffa16ef4b1a86e89ec31c9
praveengoutia <praveen.goutia@rackspace.com>
2017-08-29 22:31:51 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #File name: test.sh
 ##Format disk xvdb
-TGTDEV=xvdb
+TGTDEV=/dev/xvdb
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
   o # clear the in memory partition table
   n # new partition
   p # primary partition
   1 # partition number 1
-    # default - start at beginning of disk 
+    # default - start at beginning of disk
   +100G # /u01 partition
   n # new partition
   p # primary partition
@@ -19,34 +19,34 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
 EOF
 
 ##Format disk xvdc
-TGTDEV=xvdc
+TGTDEV=/dev/xvdc
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
   o # clear the in memory partition table
   n # new partition
   p # primary partition
   1 # partition number 1
-    # default - start at beginning of disk 
-	# default, extend partition to end of disk
+    # default - start at beginning of disk
+        # default, extend partition to end of disk
   p # print the in-memory partition table
   w # write the partition table
   q # and we're done
 EOF
 
 ##Format disk xvdd
-TGTDEV=xvdd
+TGTDEV=/dev/xvdd
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
   o # clear the in memory partition table
   n # new partition
   p # primary partition
   1 # partition number 1
-    # default - start at beginning of disk 
-	# default, extend partition to end of disk
+    # default - start at beginning of disk
+        # default, extend partition to end of disk
   p # print the in-memory partition table
   w # write the partition table
   q # and we're done
 EOF
 
-##Create Swap 
+##Create Swap
 mkswap /dev/xvdb2>/tmp/swapon.1
 swapuuid=`grep UUID /tmp/swapon.1|cut -f2 -d =`
 swapon -U ${swapuuid}
