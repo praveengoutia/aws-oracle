e34aa39e6f7723c2731bc4ba5dbe507b08876743
praveengoutia <praveen.goutia@rackspace.com>
2017-08-30 12:02:56 +1000

    Update createdbmounts.sh

--- createdbmounts.sh
+++ createdbmounts.sh
@@ -5,7 +5,6 @@
 
 diskname=$1
 mountpointname=$2
-vsize=$3
 volgrpname=$mountpointname"_vg"
 lvdiskname=$mountpointname"_lv"
 
@@ -14,8 +13,8 @@ mkdir "/$mountpointname"
 ##Mount /u01
 pvcreate $diskname
 vgcreate $volgrpname $diskname
-lvcreate -L $vsize"G" -n $lvdiskname $volgrpname
+vextentnum=`vgdisplay $volgrpname|grep 'Total PE'|awk '{print $3}'`
+lvcreate -l $vextentnum -n $lvdiskname $volgrpname
 mkfs.ext4 /dev/$volgrpname/$lvdiskname
-
 echo "/dev/mapper/$volgrpname-$lvdiskname   /$mountpointname 		ext4 		defaults	0 0">>/etc/fstab
 mount -a
