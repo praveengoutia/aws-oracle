4b68f53c67a4f341493ae149d5bca6716aeb5c05
praveengoutia <praveen.goutia@rackspace.com>
2017-08-30 11:31:33 +1000

    Create createdbmounts.sh

---
+++ createdbmounts.sh
@@ -0,0 +1,21 @@
+###
+#Script Name: createdbmounts.sh
+#
+###
+
+diskname=$1
+mountpointname=$2
+vsize=$3
+volgrpname=$mountpointname"_vg"
+lvdiskname=$mountpointname"_lv"
+
+mkdir "/$mountpointname"
+
+##Mount /u01
+createpv $diskname
+vgcreate $volgrpname $diskname
+lvcreate -L $vsize"G" -n $lvdiskname $volgrpname
+mkfs.ext4 /dev/$volgrpname/$lvdiskname
+
+echo "/dev/mapper/$volgrpname-$lvdiskname   /$mountpointname 		ext4 		defaults	0 0">>/etc/fstab
+mount -a
