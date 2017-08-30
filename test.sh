5dae7d9ead7caf8a6592904bd3edc39375ef4a06
praveengoutia <praveen.goutia@rackspace.com>
2017-08-30 11:32:42 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #File name: test.sh
+
+
+declare SCRIPT_DIR="/root/rsoradba"
+declare INPUTPARAMFILE="$SCRIPT_DIR/inputparameters.txt"
+declare SCREATEDBMOUNT="$SCRIPT_DIR/createdbmounts.sh"
+
+declare URL_BASE="https://raw.githubusercontent.com/praveengoutia/aws-oracle/master"
+
+## Download required scripts
+wget "$URL_BASE/createdbmounts.sh"
+chmod 755 "$SCREATEDBMOUNT" 
+
 ##Format disk xvdb
 TGTDEV=/dev/xvdb
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
@@ -54,11 +66,23 @@ echo "UUID=$swapuuid    swap		swap    defaults        0 0">>/etc/fstab
 
 
 ##Mount /u01
-mkfs.ext4  /dev/xvdb1
 createpv /dev/xvdb1
 vgcreate vgu01 /dev/xvdb1
 lvcreate -L 100G -n lvu01 vgu01
 mkfs.ext4 /dev/vgu01/lvu01
-
+mkdir /u01
 echo "/dev/mapper/vgu01-lvu01   /u01 		ext4 		defaults	0 0">>/etc/fstab
 mount -a
+
+##Check type of storage choose for Oracle 
+v_storage_type=`grep CFAESTORAGETYPE $INPUTPARAMFILE|cut -f2 -d ':'` 
+if [ "$v_storage_type" = "File System" ]; then
+##call script to create data and fra mount point
+	v_datasize=`grep CFAFDATASIZE $INPUTPARAMFILE|cut -f2 -d ':'|sed "s/^0*//"` 
+	v_frasize=`grep CFAGFRASIZE $INPUTPARAMFILE|cut -f2 -d ':'|sed "s/^0*//"` 
+	$SCREATEDBMOUNT "/dev/xvdc1" DATA $v_datasize
+	$SCREATEDBMOUNT "/dev/xvdd1" FRA $v_frasize
+else
+##call script to create data and fra disks
+fi
+
