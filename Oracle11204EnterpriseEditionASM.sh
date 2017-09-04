301185818f83438cf481a47b5536d95484bcc26a
praveengoutia <praveen.goutia@rackspace.com>
2017-09-05 08:50:59 +1000

    Add files via upload

--- Oracle11204EnterpriseEditionASM.sh
+++ Oracle11204EnterpriseEditionASM.sh
@@ -242,8 +242,9 @@ if [ "$CFPARAM_ORASTORAGETYPE" = "ASM" ]; then
 	$RUNUSER -l $GI_USER -c "export ORACLE_SID=+ASM;export ORACLE_HOME=$V_GRIDHOME;export PATH=$V_GRIDHOME/bin:$PATH;$V_GRIDHOME/bin/sqlplus /nolog @/tmp/addASMDiskgroups.sql"
 	V_ASMSPFILE=`cat /tmp/spfilename.txt|$SED '/^$/d'`
 	$RUNUSER -l $GI_USER -c "export ORACLE_HOME=$V_GRIDHOME;export PATH=$V_GRIDHOME/bin:$PATH;$V_GRIDHOME/bin/srvctl modify asm -p $V_ASMSPFILE"
-	$RUNUSER -l $GI_USER -c "export ORACLE_HOME=$V_GRIDHOME;export PATH=$V_GRIDHOME/bin:$PATH;$V_GRIDHOME/bin/srvctl stop asm"	
+	$RUNUSER -l $GI_USER -c "export ORACLE_HOME=$V_GRIDHOME;export PATH=$V_GRIDHOME/bin:$PATH;$V_GRIDHOME/bin/srvctl stop asm -f"	
 	$RUNUSER -l $GI_USER -c "export ORACLE_HOME=$V_GRIDHOME;export PATH=$V_GRIDHOME/bin:$PATH;$V_GRIDHOME/bin/srvctl start asm"
+	$RUNUSER -l $GI_USER -c "export ORACLE_HOME=$V_GRIDHOME;export PATH=$V_GRIDHOME/bin:$PATH;$V_GRIDHOME/bin/crsctl status res -t"	
 	rm -f  /tmp/addASMDiskgroups.sql /tmp/spfilename.txt
 	
 fi
